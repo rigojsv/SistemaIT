a3cab7742e935d9cc6945dd54912fb10330dc518
rigojsv <rigojsv.rsv@gmail.com>
2025-04-06 01:16:26 -0600

    added reports

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const session = require('express-session');
 const passport = require('passport');
 const flash = require('connect-flash');
 require('dotenv').config();
+const html2canvas = require('html2canvas-pro');
 
 
 
--- routes/reports.js
+++ routes/reports.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const { validatesession, ValidateSessionadmin } = require('../auth/auth');
+const conn = require('../db/connection');
 
 router.get('/', ValidateSessionadmin, (req, res) => {
-    res.render('reports/index', {
-        auth: req.isAuthenticated(),
-        user: req.user
+
+    conn.query(' SELECT u.nombre, COUNT(*) AS total FROM asignaciones a JOIN usuarios u ON u.id_usuario = a.id_usuario GROUP BY u.nombre', (err, usuarios) => {
+        if (err) {
+            console.error(err);
+            return res.send('Error al obtener los reportes');    
+        }
+
+        conn.query('select estado, count(distinct id_equipo) as total from reparaciones group by estado;', (err, estados) => {
+            if(err){
+                console.error(err);
+                return res.send('Error al obtener los reportes');    
+            }
+
+            conn.query("SELECT 'Asignados' AS estado, COUNT(DISTINCT e.id_equipo) AS total FROM equipos e JOIN asignaciones a ON e.id_equipo = a.id_equipo UNION SELECT 'No asignados' AS estado, COUNT(e.id_equipo) AS total FROM equipos e LEFT JOIN asignaciones a ON e.id_equipo = a.id_equipo WHERE a.id_asignacion IS NULL;", (err, asignaciones) => {
+                if(err){
+                    console.error(err);
+                    return res.send('Error al obtener los reportes');    
+                }
+                res.render('reports/index', {
+                    auth: req.isAuthenticated(),
+                    user: req.user,
+                    messages: req.flash(),
+                    usuarios,
+                    estados,
+                    asignaciones
+                });
+            });
+        });
     });
 });
 
